76d581e56ab98a07cf44ada91112aa70c26fe138
TechnoVisionDev <thomasanthonypeters@gmail.com>
2023-05-28 15:47:49 -0700

    Add metadata and favicon

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,10 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>DrugSafe</title>
+        <meta name="description" content="A modern harm reduction website."/>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicons/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" href="/favicons/apple-touch-icon?<generated>" type="image/<generated>" sizes="<generated>" />
       </Head>
       <SSRProvider>
         <Component {...pageProps} />
