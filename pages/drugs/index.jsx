7cf9de7b8ba9e6add25e485434f3af091d075626
TechnoVisionDev <thomasanthonypeters@gmail.com>
2023-05-28 18:09:56 -0700

    Add drug search

--- pages/drugs/index.jsx
+++ pages/drugs/index.jsx
@@ -1,11 +1,60 @@
+import React, { useState } from 'react';
+import Link from 'next/link';
+import Form from 'react-bootstrap/Form';
 import Navbar from "../../components/Navbar";
+import styles from './index.module.css';
 
-const Drugs = () => {
+const Drugs = ({ drugs }) => {
+
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const filteredDrugs = drugs.filter((drug) => {
+        return drug.name.toLowerCase().startsWith(searchTerm.toLowerCase()) ||
+            drug.aliases.some(alias =>
+                alias.toLowerCase().startsWith(searchTerm.toLowerCase())
+            )
+    });
+
     return (
         <>
-            <Navbar/>
+            <Navbar />
+            <div className="pageContainer">
+                <section className="headerContainer">
+                    <img className="image" src="https://cdn-icons-png.flaticon.com/512/2833/2833242.png" alt="Icon for drugs" />
+                    <h1 className="header">Search Drugs</h1>
+                    <p className={styles.subtitle}>
+                        <b>{"-- { "}</b>
+                        Find harm reduction guides for any drug
+                        <b>{" } --"}</b>
+                    </p>
+                </section>
+                <Form.Control className={styles.search} onChange={(e) => setSearchTerm(e.target.value)} type="search" placeholder="Search"  aria-label="Search" />
+                <div className={styles.gridContainer}>
+                    {filteredDrugs.map((drug) => (
+                        <Link href={`/drugs/${drug.name.toLowerCase()}`} className={styles.drugBox} key={drug.name}>
+                            <img src={drug.icon} alt={drug.name} className={styles.drugIcon} />
+                            <h2 className={styles.drugName}>{drug.name}</h2>
+                        </Link>
+                    ))}
+                </div>
+            </div>
         </>
     );
+}
+
+export async function getServerSideProps() {
+    // Retrieve drug data
+    const res = await fetch(`${process.env.URL}/api/drugs`)
+    const drugs = await res.json()
+    if (!res.ok) {
+        throw new Error(data.message);
+    }
+
+    return {
+        props: {
+            drugs,
+        },
+    }
 }
 
 export default Drugs;
