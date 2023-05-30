a01059e30c32bf3a03f70e9940095c5bd5512b24
TechnoVisionDev <thomasanthonypeters@gmail.com>
2023-05-30 11:26:16 -0700

    Redesign landing page

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,18 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <section className="headerContainer">
-        <img className="image mt-5" src="https://cdn-icons-png.flaticon.com/512/3790/3790110.png" alt="Icon for boofing" />
-        <h1 className="header">DrugSafe</h1>
-        <p className="subtitle">
-          <b>{"-- { "}</b>
-          Harm reduction, made simple
-          <b>{" } --"}</b>
-        </p>
-        <div className={styles.container}>
-          <Button href="/drugs" variant="primary">Get Started</Button>
-          <Button href="https://discord.gg/s8CFunneAg" target="_blank" rel="noopener noreferrer" variant="primary">Join Our Community</Button>
+      <section className={styles.container}>
+        <div className={styles.textContainer}>
+          <h1 className={styles.header}>The Future of <span className={styles.span}>Accessible</span><br className={styles.br}/> Harm Reduction</h1>
+          <p className={styles.subtitle}>
+            We're committed to providing simplified, readily available harm reduction resources. Life-saving information is just a click away, regardless of the device you use.
+          </p>
+          <div className={styles.buttonContainer}>
+            <Button href="/drugs" variant="primary">Get Started</Button>
+            <Button href="https://discord.gg/s8CFunneAg" target="_blank" rel="noopener noreferrer" variant="outline-primary">Join Our Community</Button>
+          </div>
         </div>
+        <img className={styles.image} src="https://cdn-icons-png.flaticon.com/512/3790/3790110.png" alt="Icon for boofing" />
       </section>
     </>
   )
