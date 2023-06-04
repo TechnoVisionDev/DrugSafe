f5c850886a93b03c0befbf27897f439cb67fada4
TechnoVisionDev <thomasanthonypeters@gmail.com>
2023-06-03 19:25:58 -0700

    Add LSD guide

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -45,6 +45,7 @@ function NavScrollExample() {
               <NavDropdown.Item href="/drugs/benzodiazepines">Benzodiazepines</NavDropdown.Item>
               <NavDropdown.Item href="/drugs/cannabis">Cannabis</NavDropdown.Item>
               <NavDropdown.Item href="/drugs/kratom">Kratom</NavDropdown.Item>
+              <NavDropdown.Item href="/drugs/lsd">LSD</NavDropdown.Item>
               <NavDropdown.Item href="/drugs/methamphetamine">Methamphetamine</NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="Guides" id="navbarScrollingDropdown">
